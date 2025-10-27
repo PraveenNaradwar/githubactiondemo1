ae3b9cc711893ff47adc370fe6ea886bc88e9a76
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2025-10-27 22:21:02 +0530

    testingdemo1

--- extract.py
+++ extract.py
@@ -3,9 +3,9 @@ import pandas as pd
 print("Extract data from Mysql database")
 
 data={
-    "id":[101,102,103],
-    "name":['sanjay','priyanka','komal'],
-    "age":[25,26,27]
+    "id":[101,102,103,104],
+    "name":['sanjay','priyanka','komal','priti'],
+    "age":[25,26,27,28]
 }
 
 df=pd.DataFrame(data)
